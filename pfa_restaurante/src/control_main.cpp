a7421f01b80b192d795e4a456a1aeca09d289052
rumbahuh <r.castilla.2023@alumnos.urjc.es>
2025-05-07 15:24:25 +0200

    Cmake fixed. Most important err: nav2 msgs was .h not .hpp. dependencies fixed

--- pfa_restaurante/src/control_main.cpp
+++ pfa_restaurante/src/control_main.cpp
@@ -1,14 +1,12 @@
 #include "rclcpp/rclcpp.hpp"
 
-#include "controlper/ControlNode.hpp"
-
 int main(int argc, char * argv[])
 {
   rclcpp::init(argc, argv);
 
-  auto node = std::make_shared<controlper::ControlNode>();
+ // auto node = std::make_shared<controlper::ControlNode>();
 
-  rclcpp::spin(node);
+ // rclcpp::spin(node);
 
   rclcpp::shutdown();
   return 0;
